e4ad32f78255155f536dcca65cb89c9e0267ace5
luheng336655 <80048693+luheng336655@users.noreply.github.com>
2021-09-23 08:58:30 +0800

    Add files via upload
    学生信息模块化

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//main.cpp
+#include "student.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	Student st;
+	st.set_value();
+	st.display();
+	return 0;
+}
---
+++ student.h
@@ -0,0 +1,12 @@
+#pragma once
+//student.h
+class Student
+{
+public:
+	void set_value();
+	void display();
+private:
+	int num;
+	char name[20];
+	char sex;
+};
